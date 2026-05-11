48d9ee1d7970dd6aaf86e456c5251de7ba562457
Irfan Ansari <irfanansari2114@gmail.com>
2026-05-11 13:31:52 +0530

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,65 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+interface Env {
+  DELIVERY_TOKEN: string;
+}
+
+interface SuccessResponse {
+  success: boolean;
+  msg: string;
+  data: {
+    tat: number;
+  };
+}
+
+interface ErrorResponse {
+  success: boolean;
+  msg: string;
+  data: string;
+}
+
+const ORIGIN_PINCODE = "201301";
+const MODE_OF_TRANSPORT = "E";
+const BASE_URL = "https://track.delhivery.com/api/dc/expected_tat";
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(request: Request, env: Env): Promise<Response> {
+    try {
+      const { searchParams } = new URL(request.url);
+      const destinationPincode = searchParams.get("pincode");
+
+      if (!destinationPincode) {
+        return Response.json({ error: "Pincode is required" }, { status: 400 });
+      }
+
+      // Call delivery API
+      const response = await fetch(
+        `${BASE_URL}?origin_pin=${ORIGIN_PINCODE}&destination_pin=${destinationPincode}&mot=${MODE_OF_TRANSPORT}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Token ${env.DELIVERY_TOKEN}`,
+          },
+        },
+      );
+
+      const data: SuccessResponse | ErrorResponse = await response.json();
+      if (!response.ok) {
+        return Response.json(
+          { ...(data as ErrorResponse) },
+          { status: response.status },
+        );
+      }
+
+      return Response.json({ ...(data as SuccessResponse) }, { status: 200 });
+    } catch (error) {
+      return Response.json(
+        {
+          error: "Internal server error",
+          message: error instanceof Error ? error.message : "Unknown error",
+        },
+        { status: 500 },
+      );
+    }
+  },
 } satisfies ExportedHandler<Env>;
